d170e58611c29192aebde9d41c6e1e3f120753e2
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-02-15 10:50:02 -0500

    pulled off Nav component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React, { Component } from 'react';
-
-// Normalizes string as a slug - a string that is safe to use
-// in both URLs and html attributes
 import slugify from 'slugify';
-
 import './App.css';
+import Nav from './Nav/Nav';
 
-// This object will allow us to
-// easily convert numbers into US dollar values
 const USCurrencyFormat = new Intl.NumberFormat('en-US', {
   style: 'currency',
   currency: 'USD'
@@ -97,9 +92,7 @@ class App extends Component {
 
     return (
       <div className="App">
-        <header>
-          <h1>ELF Computing | Laptops</h1>
-        </header>
+        <Nav />
         <main>
           <form className="main__form">
             <h2>Customize your laptop</h2>
---
+++ src/Nav/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Nav.css';
+
+function Nav (){
+    return (
+        <header>
+          <h1>ELF Computing | Laptops</h1>
+        </header>
+    )
+}
+
+export default Nav;
